2de1b66066cf1bb9782a21e20f6b46b865127a47
Daniel Alvarez <elquimeras@gmail.com>
2023-12-06 11:16:06 -0300

    Refactor index file

--- api/index.py
+++ api/index.py
@@ -1,10 +1,5 @@
-from flask import Flask
-app = Flask(__name__)
+from .app import create_app
 
-
-@app.route("/api/hello")
-def hello_world():
-    return "<p>Hello, World!</p>"
-
-if __name__ == "__main__":
+if __name__ == '__main__':
+    app = create_app()
     app.run(debug=True)
