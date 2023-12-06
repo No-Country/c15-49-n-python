b82f932f9d1cb0283965db3468332e00f28ab6c8
Daniel Alvarez <elquimeras@gmail.com>
2023-12-06 10:57:03 -0300

    Init models & user model

---
+++ api/app/models/user.py
@@ -0,0 +1,14 @@
+from .. import db
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(255), unique=True, nullable=False)
+    password = db.Column(db.String(255), nullable=False)
+    first_name = db.Column(db.String(255))
+    last_name = db.Column(db.String(255))
+    location = db.Column(db.String(255))
+    is_active = db.Column(db.Boolean, default=True)
+
+    def __repr__(self):
+        return f"<User {self.username}>"
