c6a343ce10fd8b5428b2bfdf44c09a40d19d56d3
pablinux87 <pabloratero@gmail.com>
2023-12-06 23:55:17 -0300

    Se agrega la entidad job_detail por parte de David

--- api/app/__init__.py
+++ api/app/__init__.py
@@ -19,6 +19,6 @@ def create_app(config_name='default'):
 
     app.register_blueprint(api)
 
-    from .models import User, Job, Relation_user_job
+    from .models import User, Job, Relation_user_job, Job_detail
 
     return app
--- api/app/models/__init__.py
+++ api/app/models/__init__.py
@@ -1,3 +1,4 @@
 from .user import User
 from .job import Job
 from .relation_user_job import Relation_user_job
+from .job_detail import Job_detail
