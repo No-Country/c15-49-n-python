7f323bab13080e8004e5f5cf9ac885850ce8fb79
Hernan Casasola <67485423+GuidoMaxier@users.noreply.github.com>
2023-12-13 19:46:22 -0300

    Add profile_image to User model

--- api/app/auth/views.py
+++ api/app/auth/views.py
@@ -1,7 +1,15 @@
 # /auth/views.py
-from flask import request, jsonify
+from flask import request, jsonify, redirect, url_for, flash
 from . import auth
 from ..models import User, db
+
+# 
+from flask_login import login_user, logout_user, login_required, current_user
+from . import auth
+from ..models import User
+
+
+import re
 
 @auth.route("/api/register", methods=['POST'])
 def register():
@@ -14,6 +22,14 @@ def register():
 
     if not email or not password:
         return jsonify({'message': 'Datos incompletos'}), 400
+
+    # Verificar la longitud de la contraseña
+    if not (8 <= len(password) <= 16):
+        return jsonify({'message': 'La contraseña debe tener entre 8 y 16 caracteres'}), 400
+
+    # Verificar que la contraseña contenga al menos una mayúscula, una minúscula y un número
+    if not re.search(r'[A-Z]', password) or not re.search(r'[a-z]', password) or not re.search(r'\d', password):
+        return jsonify({'message': 'La contraseña debe contener al menos una mayúscula, una minúscula y un número'}), 400
 
     existing_user = User.query.filter_by(email=email).first()
 
@@ -29,6 +45,7 @@ def register():
     return jsonify({'message': 'Usuario registrado exitosamente'}), 201
 
 
+
 @auth.route("/api/login", methods=['POST'])
 def login():
     data = request.get_json()
@@ -39,25 +56,19 @@ def login():
     user = User.query.filter_by(email=email).first()
     
     if user and user.check_password(password):
-        # Tu lógica de inicio de sesión aquí
+        # inicio de sesión aquí
+        login_user(user)
         return jsonify({'message': 'Inicio de sesión exitoso', 'user_id': user.id, 'email': user.email}), 200
     else:
         return jsonify({'message': 'Correo electrónico o contraseña inválidos'}), 401
 
 
 
-
+### solo para ver si esta bien la ruta
 @auth.route("/")
 def index():
     return jsonify({'users': "hola"})
 
-
-
-# /auth/views.py
-from flask import request, jsonify, redirect, url_for, flash
-from flask_login import login_user, logout_user, login_required, current_user
-from . import auth
-from ..models import User
 
 
 @auth.route("/api/logout")
@@ -67,16 +78,36 @@ def logout():
     return jsonify({'message': 'Cierre de sesión exitoso'}), 200
 
 
-
+# /auth/views.py
 @auth.route("/api/user_info")
 @login_required
 def user_info():
-    # Accede a la información del usuario actualmente autenticado
     user_info = {
         'user_id': current_user.id,
         'email': current_user.email,
         'first_name': current_user.first_name,
         'last_name': current_user.last_name,
         'location': current_user.location,
+        'profile_image': current_user.profile_image,
     }
     return jsonify(user_info), 200
+
+
+
+### Editar Perfil ####
+
+@auth.route("/api/edit_profile", methods=['PUT'])
+@login_required
+def edit_profile():
+    data = request.get_json()
+
+    current_user.first_name = data.get('first_name', current_user.first_name)
+    current_user.last_name = data.get('last_name', current_user.last_name)
+    current_user.location = data.get('location', current_user.location)
+    current_user.profile_image = data.get('profile_image', current_user.profile_image)
+
+    db.session.commit()
+
+    return jsonify({'message': 'Perfil actualizado exitosamente'}), 200
+
+
