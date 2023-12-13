85f74f76000bd349556377317c4ab61e62d84035
Daniel Alvarez <elquimeras@gmail.com>
2023-12-13 14:52:22 -0300

    Add PoviderCards & IdeaCards to landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,77 @@
 import Banner from './components/banner';
-import Card from './components/card';
+import ProviderCard from './components/provider-card';
+import IdeaCard from './components/idea-card';
 
 export default function Home() {
   return (
     <>
-      <Banner></Banner>
+      <Banner />
       <section id="main__content" className="mx-auto max-w-7xl mt-5">
         <h2 className="text-2xl font-medium">¿Con qué podemos ayudarte hoy?</h2>
-        <div id="cards" className="mt-10 flex flex-wrap justify-center">
-          <Card>Proveedor 1</Card>
-          <Card>Proveedor 2</Card>
-          <Card>Proveedor 3</Card>
-          <Card>Proveedor 4</Card>
-          <Card>Proveedor 5</Card>
-          <Card>Proveedor 6</Card>
-        </div>
+        <section id="providers" className="mt-10 flex flex-wrap justify-center">
+          {/* ToDo: Get providers from API */}
+          <ProviderCard
+            name="El viejo Gepetto"
+            job="Carpintero"
+            address="Maipú, Santiago"
+            image="/images/providers/carpintero.jpeg"
+          />
+          <ProviderCard
+            name="Jhon Smith"
+            job="Fontanero"
+            address="Lampa, Santiago"
+            image="/images/providers/fontanero.jpeg"
+          />
+          <ProviderCard
+            name="Jardines Babilonios"
+            job="Jardinero"
+            address="Lampa, Santiago"
+            image="/images/providers/jardinero.jpeg"
+          />
+          <ProviderCard
+            name="La Pinturería"
+            job="Pintor"
+            address="Santiago, Santiago"
+            image="/images/providers/pintor.jpeg"
+          />
+          <ProviderCard
+            name="Pedro Perez"
+            job="Reparaciones"
+            address="Vitacura, Santiago"
+            image="/images/providers/reparaciones.jpeg"
+          />
+          <ProviderCard
+            name="Juan Mecanico"
+            job="Mecanica Automotríz"
+            address="Providencia, Santiago"
+            image="/images/providers/mecanico.jpeg"
+          />
+        </section>
+
+        <h2 className="text-2xl font-medium">Ideas para mejorar tu hogar</h2>
+        <section id="ideas" className="mt-10 flex flex-wrap justify-center">
+          {/* ToDo: Get providers from API */}
+          <IdeaCard
+            title="Domando el frió: 5 Trucos para decorar tu casa en invierno"
+            description="Lorem ipsum dolor sit amet"
+            image="/images/ideas/idea-1.webp"
+          />
+          <IdeaCard
+            title="¡Reenamórate de tu casa! 7 razones por las que vale la pena contratar un ..."
+            description="Lorem ipsum dolor sit amet"
+            image="/images/ideas/idea-2.webp"
+          />
+          <IdeaCard
+            title="Living pequeño, grandes posibilidades: 10 trucos de decoración"
+            description="Lorem ipsum dolor sit amet"
+            image="/images/ideas/idea-1.webp"
+          />
+          <IdeaCard
+            title="Domando el frió: 5 Trucos para decorar tu casa en invierno"
+            description="Lorem ipsum dolor sit amet"
+            image="/images/ideas/idea-3.webp"
+          />
+        </section>
       </section>
     </>
   );
