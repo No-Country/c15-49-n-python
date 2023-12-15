500cad749f3f1ecc9935ebd9cfe5ad51e5837c89
Daniel Alvarez <elquimeras@gmail.com>
2023-12-15 10:46:20 -0300

    Add a handler to api

--- api/index.py
+++ api/index.py
@@ -1,4 +1,11 @@
 from .app import create_app
+from http.server import BaseHTTPRequestHandler
+
+
+class handler(BaseHTTPRequestHandler):
+    app = create_app()
+    app.run(debug=True)
+
 
 if __name__ == '__main__':
     app = create_app()
