28931a5e59ed148de466048fa5b5f207108e5c95
Daniel Alvarez <elquimeras@gmail.com>
2023-12-15 11:09:32 -0300

    Improve index.py

--- api/index.py
+++ api/index.py
@@ -1,12 +1,4 @@
 from .app import create_app
-from http.server import BaseHTTPRequestHandler
 
-
-class handler(BaseHTTPRequestHandler):
-    app = create_app()
-    app.run(debug=True)
-
-
-if __name__ == '__main__':
-    app = create_app()
-    app.run(debug=True)
+app = create_app()
+app.run(debug=True)
