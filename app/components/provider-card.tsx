0ad5d9ffcd26e61233fb12819a01c09ab79d0af6
Diego Iacub <105656485+sombrerodiego@users.noreply.github.com>
2023-12-20 23:13:35 -0300

    add avatar and style

--- app/components/provider-card.tsx
+++ app/components/provider-card.tsx
@@ -5,15 +5,17 @@ export default function ProviderCard({
   name,
   job,
   address,
+  avatar,
   image,
 }: {
   name: string;
   job: string;
   address: string;
+  avatar: string;
   image: string;
 }) {
   return (
-    <div className="m-1 p-1 relative w-80 h-80 bg-white rounded-lg shadow-md p-4 transition duration-300 transform hover:scale-105">
+    <div className="m-1 p-1 relative w-80 h-90 bg-white rounded-lg shadow-md p-4 transition duration-300 transform hover:scale-105">
       <div className="h-40 relative">
         <Image
           className="object-cover w-full h-full rounded-t-lg"
@@ -21,6 +23,16 @@ export default function ProviderCard({
           alt="Proveedor"
           layout="fill"
         />
+      <div className="profile-image w-24 h-24 rounded-full overflow-hidden absolute botton-0 left-0 border-8" style={{ borderColor: "rgba(142, 85, 235, 0.8)" }}>
+      <Image
+          className="h-full w-auto"
+          src={avatar}
+          alt="Proveedor"
+          width={0}
+          height={0}
+          sizes="100vw"
+        />
+      </div>
       </div>
 
       <div className="p-1 flex">
@@ -45,6 +57,7 @@ export default function ProviderCard({
             <svg className="w-6 h-6 text-gray-500 fill-current" viewBox="0 0 24 24">
               <path d="M12 17.27L18.18 21L16.54 13.97L22 9.24L14.81 8.63L12 2L9.19 8.63L2 9.24L7.46 13.97L5.82 21L12 17.27Z" />
             </svg>
+            
           </div>
 
           <div className="font-medium text-lg">{name}</div>
@@ -54,7 +67,7 @@ export default function ProviderCard({
 
         <div className="flex justify-center mt-4">
           <button className="px-4 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-600 transition-colors duration-300">
-            Ver más
+            Arreglar YA
           </button>
         </div>
       </div>
