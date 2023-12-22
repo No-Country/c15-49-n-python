369913156489f7768f0ddf6863c7877294bd0037
Diego Iacub <105656485+sombrerodiego@users.noreply.github.com>
2023-12-21 21:37:55 -0300

    correct profile page

---
+++ app/profile/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import UserProfile from '../components/user-profile'
+
+<UserProfile 
+  name="El viejo Gepetto"
+  job="Carpintero"
+  address="Maipú, Santiago"
+  avatar='/images/providers/avatar.png'
+  image="/images/providers/carpintero.jpeg"
+/>
